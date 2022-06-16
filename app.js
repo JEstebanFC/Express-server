a6d6981b7d0b4d45139e0e34204a90164b032c1c
Jaime Farina <fffarinha@gmail.com>
2022-06-16 15:38:25 +1200

    Init

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
 const PORT = 3000
+
+const express = require('express')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const app = express()
+app.use(express.json())
 
 app.get('/', function(req, res){
     res.send('<h1>Hi guys</h1>')
+})
+
+app.post('/image', upload.single('image'), function(req, res){
+    const body = req.body
+    const file = req.file
+    console.log(file)
+    res.send('Hi there')
 })
 
 app.listen(PORT)
